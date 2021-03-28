d298dcba8d596e00a87ef16e74811fba0c63d2bf
ploqop <ploqop@gmail.com>
2021-03-28 23:30:14 +0300

    тупой трэвис считает предупреждения за ошибки ну и дурак

--- app/main.c
+++ app/main.c
@@ -9,7 +9,11 @@ int main()
     printf("Hello World!\n");
     printf("ax^2+bx+c=0\n");
     printf("Input a, b and c: \n");
-    scanf("%lf%lf%lf", &a, &b, &c);
+    if(scanf("%lf%lf%lf", &a, &b, &c) != 3)
+    {
+        printf("Ошибка ввода данных");
+        return 0;
+    }
     square(a, b, c);
     return myfunc(2);
 }
