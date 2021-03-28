333406499140a2b006f66070ddfca71e13d655b0
ploqop <ploqop@gmail.com>
2021-03-28 23:25:16 +0300

    доделал тестики

--- tests/square.c
+++ tests/square.c
@@ -7,6 +7,7 @@
 solution square(double a, double b, double c)
 {
     solution POPUSHb;
+
     POPUSHb.D = POPUSHb.roots = 0;
 
     POPUSHb.D = powl(b,2) - 4*a*c;
@@ -19,15 +20,15 @@ solution square(double a, double b, double c)
     }
     else if (POPUSHb.D == 0)
     {
-        POPUSHb.x_1 = -b/2*a;
+        POPUSHb.x_1 = -b/(2*a);
         POPUSHb.roots = 1;
         printf("Discriminant = %f, there is %d root: x = %f\n",POPUSHb.D,POPUSHb.roots,POPUSHb.x_1);
         return POPUSHb;
     }
     else
     {
-        POPUSHb.x_1 = (-b+sqrtl(POPUSHb.D))/2*a;
-        POPUSHb.x_2 = (-b-sqrtl(POPUSHb.D))/2*a;
+        POPUSHb.x_1 = (-b+sqrtl(POPUSHb.D))/(2*a);
+        POPUSHb.x_2 = (-b-sqrtl(POPUSHb.D))/(2*a);
         POPUSHb.roots = 2;
         printf("Discriminant = %f, %f > 0, there are %d roots: x1 = %f, x2 = %f\n",POPUSHb.D,POPUSHb.D,POPUSHb.roots,POPUSHb.x_1,POPUSHb.x_2);
         return POPUSHb;
--- tests/square_test.h
+++ tests/square_test.h
@@ -7,60 +7,40 @@ extern "C" {
 #include "square.h"
 }
 
-TEST(square, no_sol) {
-    solution POPUSHb = square(1, 2, 6);
-    EXPECT_EQ(0, POPUSHb.roots);
+TEST(square, test_1) {
+    solution POPUSHb = square(2, 3, 4);
+    EXPECT_EQ(-23, POPUSHb.D);
 }
-TEST(square, one_sol) {
+
+TEST(square, test_2) {
     solution POPUSHb = square(4, 4, 1);
+    EXPECT_EQ(0, POPUSHb.D);
     EXPECT_EQ(-0.5, POPUSHb.x_1);
 }
 
-TEST(square, two_sol) {
-    solution POPUSHb = square(2, 5, 2);
-    EXPECT_EQ(-2, POPUSHb.x_1);
-    EXPECT_EQ(-0.5, POPUSHb.x_2);
-}
-TEST(square, test4_negative){
-    solution POPUSHb = square(0, 1, 1);
-    EXPECT_EQ(-1, POPUSHb.x_1);
-
-}
-TEST(square, test5_negative){
-    solution POPUSHb = square(1, 0, -1);
-    EXPECT_EQ(-1, POPUSHb.x_1);
-    EXPECT_EQ(1, POPUSHb.x_2);
-
-}
-TEST(square, test6_negative){
-    solution POPUSHb = square(1, 0, 1);
-    EXPECT_EQ(0, POPUSHb.roots);
-
-}
-TEST(square, test7_negative){
-    solution POPUSHb = square(0, 0, 1);
-    EXPECT_EQ(0, POPUSHb.roots);
-
-}
-TEST(square, test8_negative){
-    solution POPUSHb = square(0, 1, 0);
+TEST(square, test_3) {
+    solution POPUSHb = square(2, 5, 0);
+    EXPECT_EQ(25, POPUSHb.D);
     EXPECT_EQ(0, POPUSHb.x_1);
+    EXPECT_EQ(-2.5, POPUSHb.x_2);
+}
+
+TEST(square, test_4){
+    solution POPUSHb = square(2, 0, 1);
+    EXPECT_EQ(-8, POPUSHb.D);
 
 }
-TEST(square, test9_negative){
-    solution POPUSHb = square(0, 0, 0);
-    EXPECT_EQ(0, POPUSHb.roots);
 
+TEST(square, test_5){
+    solution POPUSHb = square(6, 12, -123);
+    EXPECT_EQ(3096, POPUSHb.D);
+    EXPECT_TRUE((POPUSHb.x_1 >= 3) && (POPUSHb.x_1 <= 4));
+    EXPECT_TRUE((POPUSHb.x_2 >= -6) && (POPUSHb.x_2 <= -5));
 }
-TEST(square, test10_negative){
-    solution POPUSHb = square(1, 1, 0);
-    EXPECT_EQ(-1, POPUSHb.x_1);
-    EXPECT_EQ(0, POPUSHb.x_2);
 
-}
-TEST(square, test11_negative){
-    solution POPUSHb = square(1, 0, 0);
-    EXPECT_EQ(0, POPUSHb.x_1);
-
-
+TEST(square, test_6){
+    solution POPUSHb = square(49, -31, 3);
+    EXPECT_EQ(373, POPUSHb.D);
+    EXPECT_TRUE((POPUSHb.x_1 >= 0) && (POPUSHb.x_1 <= 1));
+    EXPECT_TRUE((POPUSHb.x_2 >= 0) && (POPUSHb.x_2 <= 1));
 }
