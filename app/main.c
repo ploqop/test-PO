cbf11e31f78c4670691457e685ad774f5b7c4461
ploqop <ploqop@gmail.com>
2021-03-30 16:17:20 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -6,14 +6,17 @@
 int main()
 {
     double a, b, c;
-    printf("Hello World!\n");
+    a = 3;
+    b = 5;
+    c = 2;
+ /*   printf("Hello World!\n");
     printf("ax^2+bx+c=0\n");
     printf("Input a, b and c: \n");
     if(scanf("%lf%lf%lf", &a, &b, &c) != 3)
     {
         printf("Ошибка ввода данных");
         return 0;
-    }
+    } */
     square(a, b, c);
     return myfunc(2);
 }
